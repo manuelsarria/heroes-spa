81bc8412960065a1a0b1eb622cbf50f231a561b1
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-01-30 13:43:21 -0500

    update heroes app

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,8 +1,50 @@
+import { useMemo } from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+
+  const { id, ...rest } = useParams();
+  const navigate = useNavigate();
+
+  const hero = useMemo( () => getHeroById( id ), [ id ] );
+
+  const onNavigateBack = () => {
+    navigate(-1);
+  }
+
+  if ( !hero ) {
+    return (<Navigate to="/marvel" />)
+  }
+
   return (
-    <>
-      HeroPage
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={ `/assets/heroes/${ id }.jpg` }
+          alt={ hero.superhero } 
+          className="img-thumbnail animate__animated animate__fadeInLeft"
+        />
+      </div>
+
+      <div className="col-8">
+        <h3>{ hero.superhero }</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"> <b>Alter ego:</b> { hero.alter_ego } </li>
+          <li className="list-group-item"> <b>Publisher:</b> { hero.publisher } </li>
+          <li className="list-group-item"> <b>First appearance:</b> { hero.first_appearance } </li>
+
+          <h5> Characters </h5>
+          <p>{ hero.characters }</p>
+
+          <button
+            className="btn btn-outline-primary"
+            onClick={ onNavigateBack }
+          >
+            Regresar
+          </button>
+        </ul>
+      </div>
+    </div>
   )
 }
