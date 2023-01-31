f7cd5a473646178ce3d17aedef319a537c3cb9e4
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-01-31 16:57:58 -0500

    public and private routes

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,10 +1,13 @@
+import { AuthProvider } from "./auth/context"
 import { AppRouter } from "./router/AppRouter"
 
 
 export const HeroesApp = () => {
   return (
-    <>
-      <AppRouter />     
-    </>
+    <AuthProvider>
+
+      <AppRouter />
+       
+    </AuthProvider>
   )
 }
